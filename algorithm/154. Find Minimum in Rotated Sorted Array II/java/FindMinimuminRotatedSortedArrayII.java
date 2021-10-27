7f69ab7189959e55a394f2d3fbc17a913bd471d3
Jackson <62815830+Phantom-Shen@users.noreply.github.com>
2021-10-27 15:56:55 -0700

    Update FindMinimuminRotatedSortedArrayII.java

--- algorithm/154. Find Minimum in Rotated Sorted Array II/java/FindMinimuminRotatedSortedArrayII.java
+++ algorithm/154. Find Minimum in Rotated Sorted Array II/java/FindMinimuminRotatedSortedArrayII.java
@@ -7,7 +7,7 @@ public:
         if(nums[0] < nums[n-1]) return nums[0];
         // notice: [1,1,1,1,1,1,1] and other similar cases
         
-        while(l < r && nums[l] > nums[r]){
+        while(l < r && nums[l] > nums[r]){ // must make sure nums[l] > nums[a], otherwise we can only do linear search
             int mid = l + (r-l)/2;
             if(mid > 0 && nums[mid-1] > nums[mid]) return nums[mid];
             else if(nums[l] <= nums[mid]){
