3d3369da36be9e7acb1dd181c8fb871b8ad75de3
Jackson <62815830+Phantom-Shen@users.noreply.github.com>
2021-10-28 23:26:21 -0700

    Update CoinChange2.java

--- algorithm/518. Coin Change 2/java/CoinChange2.java
+++ algorithm/518. Coin Change 2/java/CoinChange2.java
@@ -1,7 +1,9 @@
-// very similar Coin change. 
-// Looks like we only flipped the inner loop with the outer loop. 
-// But the reason behind it is very important
-// Understand with this post: https://leetcode.com/problems/coin-change-2/discuss/176706/Beginner-Mistake%3A-Why-an-inner-loop-for-coins-doensn't-work-Java-Soln
+// knapsack problem
+// similar to Coin change
+// looks like we only flipped the inner loop with the outer loop
+// but the reason behind it is very important
+// understand with this post: 
+// https://leetcode.com/problems/coin-change-2/discuss/176706/Beginner-Mistake%3A-Why-an-inner-loop-for-coins-doensn't-work-Java-Soln
 class Solution {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount+1];
