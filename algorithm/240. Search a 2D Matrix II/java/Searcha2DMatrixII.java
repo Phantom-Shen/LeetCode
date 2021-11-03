b41b69fd9d3c693d3dfa682a4719c905b9a56091
Jackson <62815830+Phantom-Shen@users.noreply.github.com>
2021-11-02 20:35:55 -0700

    Create Searcha2DMatrixII.java

---
+++ algorithm/240. Search a 2D Matrix II/java/Searcha2DMatrixII.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length, col = matrix[0].length;
+        int i = 0, j = col-1;
+        while(i < row && j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            if(matrix[i][j] < target){
+                i += 1;
+            }
+            else{
+                j -= 1;
+            }
+        }
+        return false;
+    }
+}
