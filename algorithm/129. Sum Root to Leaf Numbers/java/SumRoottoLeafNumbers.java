09521e8bfe5ed336347415d9adbd4d59809a02e7
Jackson <62815830+Phantom-Shen@users.noreply.github.com>
2021-11-02 19:38:29 -0700

    Create SumRoottoLeafNumbers.java

---
+++ algorithm/129. Sum Root to Leaf Numbers/java/SumRoottoLeafNumbers.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        if(root.left==null && root.right==null) return root.val;
+        int left = 0, right = 0;
+        if(root.left!=null){
+            root.left.val += 10*root.val;
+            left = sumNumbers(root.left);
+        }
+        if(root.right!=null){
+            root.right.val += 10*root.val;
+            right = sumNumbers(root.right);
+        }
+        return left+right;
+    }
+}
