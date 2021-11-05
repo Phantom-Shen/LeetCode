98a55bceddced4d4f044adc4290b3e7d3e67c911
Jackson <62815830+Phantom-Shen@users.noreply.github.com>
2021-11-05 02:20:23 -0700

    Create DailyTemperatures.java

---
+++ algorithm/739. Daily Temperatures/java/DailyTemperatures.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Deque<Integer> stack = new ArrayDeque();
+        int[] result = new int[n];
+        for(int i = n-1; i>=0; i--){
+            if(i==n-1){
+                stack.addLast(i);
+                result[i] = 0;
+                continue;
+            }
+            while(!stack.isEmpty() && temperatures[i]>=temperatures[stack.peekLast()]){
+               stack.pollLast();
+            }
+            if(stack.isEmpty()){
+                result[i] = 0;
+            }
+            else{
+                result[i] = stack.peekLast()-i;
+            }
+            stack.addLast(i);
+        }
+        return result;
+    }
+}
