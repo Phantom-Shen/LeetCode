aaca118c4efcac9df66f220b96d496a5e6b6c63c
Jackson <62815830+Phantom-Shen@users.noreply.github.com>
2021-11-13 00:46:21 -0800

    Update DailyTemperatures.java

--- algorithm/739. Daily Temperatures/java/DailyTemperatures.java
+++ algorithm/739. Daily Temperatures/java/DailyTemperatures.java
@@ -1,4 +1,24 @@
-// We can improve the space complexity by using a stack variable to record the max temp on right, in stead of using a stack which uses too much space
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        for(int i = n-1, maxi = n-1; i >= 0; i--){
+            if(temperatures[i] >= temperatures[maxi]){
+                maxi = i;
+            }
+            else{
+                int step = 1;
+                while(temperatures[i+step] <= temperatures[i]){
+                    step += result[i+step];
+                }
+                result[i] = step;
+            }
+        }
+        return result;
+    }
+}
+/*
+// We can improve the space complexity by using the calculated result for searching next warmer temp, in stead of using a stack which uses too much space
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
@@ -24,3 +44,4 @@ class Solution {
         return result;
     }
 }
+*/
